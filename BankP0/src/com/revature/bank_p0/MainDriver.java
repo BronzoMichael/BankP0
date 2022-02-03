bec3ea2f28298454795845409963d6f637095429
Michael Bronzo <bronzomichael@gmail.com>
2022-02-03 01:04:02 -0500

    Planned program flow and required methods

--- BankP0/src/com/revature/bank_p0/MainDriver.java
+++ BankP0/src/com/revature/bank_p0/MainDriver.java
@@ -131,8 +131,98 @@ public class MainDriver {
 			String lpassword = consoleReader.readLine();
 
 // scan through collection of registered users and look for matching username and password
+			
+// Once logged in successfully
+			
+			manageAccount();
+			
 		}
 
-	}
+// Run this once a customer is successfully logged in
+		public static void manageAccount() throws IOException{
+				
+			System.out.print("Welcome:");  //username goes here) 
+			System.out.print("What would you like to do today?\n" + "1) Check Balance\n" + "2) Deposit Cash\n" + "3) Withdraw Cash\n" + "4) Exit\n" "> ");
+			boolean isRunning = true;
+			while (isRunning) {
+			try {
+					String userSelection = consoleReader.readLine();
+
+//					if (userSelection.equals("1")) {
+//						System.out.println("The user selected login");
+//					} else if (userSelection.equals("2")) {
+//						System.out.println("The user selected Register");
+//					} else if (userSelection.equals("3")) {
+//						System.out.println("The user selected Exit! Now exiting....");
+//					} else if (userSelection.equals("4")) {
+//						throw new IOException("Stack example.");
+//					} else {
+//						System.out.println("What on earth are you trying to tell me to do?!?!");
+//					}
+
+//					Scientist example = new Scientist("Charles", "Jester", "JesterCharles@mail.com", "jestchar", "superPassword");
+//					System.out.println(example);
+
+					switch (userSelection) {
+					case "1":
+						//checkBalance();
+						break;
+					case "2":
+						//depositCash();
+						break;
+					case "3":
+						//withdrawCash();
+					case "4":
+						System.out.println(("The user selected Exit! Now exiting...."));
+						isRunning = false;
+						break;
+					default:
+						System.out.println("What on earth are you trying to tell me to do?!?!");
+						break;
+					}
+
+					System.out.println("User selected: " + userSelection);
+
+				} catch (IOException e) { // Catch block - catches any defined exceptions and handles according
+					// TODO Auto-generated catch block
+					e.printStackTrace(); // Stuff you don't write can be thrown in the stack, LOOK for what you wrote
+					isRunning = false;
+				} finally { // Something that runs at the end of every use case
+					System.out.println("User finished selection");
+				}
+				String fusername = consoleReader.readLine();
+				}
+			}
+		
+		public static void checkBalance() throws IOException{
+			//Will return current balance in account
+			//Then will ask would you like to make another transaction
+			//send you back to manageAccount()
+		}
+		
+		public static void withdrawCash() throws IOException{
+			//Print "You have an available balance of: "
+			//Ask how much hey want to withdraw(allow down to the $1)
+			//check to make sure they do not overdraft. If they do, print an error message and ask them to enter a different amount or exit
+			//If amount is valid:
+			//Say something like "thanks, please take your cash below
+			//Update current balance
+			//send them back to manageAccount()
+			
+		}
+		
+		public static void depositCash() throws IOException{
+			//Ask how much they would like to deposit
+			//Print line saying something like please put cash in the machine
+			//Say something like Thanks! You have deposited $20. 
+			//Update current balance
+			//send back to manageAccount()
+			
+		}
+		
+			
+		}
+
+
 
 
